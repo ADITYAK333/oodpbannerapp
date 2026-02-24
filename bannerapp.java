0131a352f772062e72bebc1d673239829f4d9069
ADITYAK333 <adityakn25@gmail.com>
2026-02-24 08:30:59 +0530

    Update bannerapp.java

--- bannerapp.java
+++ bannerapp.java
@@ -1,6 +1,6 @@
 class bannerapp{
     public static void main(String[] args){
-        System.out.println("Welcome to Banner App!");
+        System.out.println("\n");
         System.out.println("   *   *   *    ");
         System.out.println("   *       * ");
         System.out.println("   *       * ");
